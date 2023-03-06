f7042e1aa1d87d303dab286a5370d1f0fbfdad08
jeskudel <115693678+jeskudel@users.noreply.github.com>
2023-03-06 17:12:11 +0100

    balas y scripts

--- Assets/disparar.cs
+++ Assets/disparar.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class disparar : MonoBehaviour
 {
@@ -8,20 +10,29 @@ public class disparar : MonoBehaviour
     public Transform puntoDisparo;
     public float velocidad = 20;
 
+    XRGrabInteractable selfobject;
+
     // Start is called before the first frame update
     void Start()
     {
+        selfobject = GetComponent<XRGrabInteractable>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        selfobject.activated += onAutomatico;
     }
 
+    public void onAutomatico()
+    {
+        GameObject bala = Instantiate(bullet, puntoDisparo.position, puntoDisparo.rotation);
+        bala.GetComponent<Rigidbody>().velocity = puntoDisparo.forward * velocidad;
+        Destroy(bala, 5);
+    }
     public void Fire()
     {
-        GameObject bala = Instantiate(bullet);
-        bala.transform.position = puntoDisparo.position;
+        GameObject bala = Instantiate(bullet,puntoDisparo.position, puntoDisparo.rotation);
         bala.GetComponent<Rigidbody>().velocity = puntoDisparo.forward * velocidad;
         Destroy(bala, 5);
     }
