cefd3c04e3d55e6e7e503e823ae87a2162e1189e
jeskudel <115693678+jeskudel@users.noreply.github.com>
2023-03-13 19:37:49 +0100

    disparo automatico

--- Assets/disparar.cs
+++ Assets/disparar.cs
@@ -9,19 +9,39 @@ public class disparar : MonoBehaviour
     public GameObject bullet;
     public Transform puntoDisparo;
     public float velocidad = 20;
+    private float fireRate = 5;
+    private float lastfired;
 
     XRGrabInteractable selfobject;
+    ActionBasedController mandoI, mandoD;
 
     // Start is called before the first frame update
     void Start()
     {
         selfobject = GetComponent<XRGrabInteractable>();
+        mandoI = GameObject.Find("LeftHand Controller").GetComponent<ActionBasedController>();
+        mandoD = GameObject.Find("RightHand Controller").GetComponent<ActionBasedController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //selfobject.activated += onAutomatico;
+        if (selfobject.isSelected)
+        {
+            Debug.Log("Esta agarrado el bojeto " + selfobject.name);
+            if ((selfobject.IsSelected(mandoD.GetComponent<XRDirectInteractor>()) && mandoD.activateAction.action.ReadValue<float>() > 0) || (selfobject.IsSelected(mandoI.GetComponent<XRDirectInteractor>()) && mandoI.activateAction.action.ReadValue<float>() > 0))
+            {
+                if (Time.time - lastfired > 1 / fireRate)
+                {
+                    lastfired = Time.time;
+                    onAutomatico();
+                }
+            }
+            else
+            {
+                Debug.Log("Arma Seleccionada pero no apretado el gatillo");
+            }
+        }
     }
 
     public void onAutomatico()
